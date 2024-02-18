ce84d1fe1a3bf0948957e0070e0875b346bcf2a9
dev1 <dev1@abc.com>
2024-02-18 18:08:28 -0500

    remote_exec

---
+++ TF_REMOTE_EXEC/create-ins.tf
@@ -0,0 +1,35 @@
+resource "aws_key_pair" "key_pair" {
+  key_name = "levelup_key"
+  public_key = file(var.PUBLIC_KEY)
+}
+
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           =  lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  tags = {
+    Name="demoinstance"
+  }
+  security_groups = var.Security_Group[0]
+
+  provisioner "file" {
+    source = "install-niginx.sh"
+    destination = "/tmp/install-niginx.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo chmod a+x /tmp/install-niginx.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/install-niginx.sh",  
+      "sudo /tmp/install-niginx.sh"
+     ]
+  }
+
+  connection {
+    host = coalesce(self.public_ip , self.private_ip)
+    type = "ssh"
+    user = var.USER_NAME
+    private_key = file(var.PRIVATE_KEY)
+  }
+}
+
---
+++ TF_REMOTE_EXEC/variables.tf
@@ -0,0 +1,35 @@
+
+variable "AWS_ACCESS_KEY" {
+  
+}
+variable "AWS_SECRET_KEY" {
+  
+}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "Security_Group" {
+  type = list
+  default = ["sg-06d8937e8335f2cd1"]
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-0b0ea68c435eb488d"
+        us-east-2 = "ami-05803413c51f242b7"
+    }
+}
+
+variable "PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "USER_NAME" {
+  default = "ec2_user"
+}
