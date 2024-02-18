d5623a080a9567998b78093d016ca7c99e7aad97
dev1 <dev1@abc.com>
2024-02-18 01:00:15 -0500

    looup

--- TF_Variables/create-ins.tf
+++ TF_Variables/create-ins.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "MyFirstInstnace" {
-  ami           =  var.AMIS
+  ami           =  lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   tags = {
     Name="demoinstance"
