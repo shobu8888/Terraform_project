955800d417f8463500c79207f5c95c628cd57caf
Sachin Gupta <47683702+shobu8888@users.noreply.github.com>
2024-02-17 22:56:26 -0500

    deleted sec kuys

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 # Configure the AWS Provider
 provider "aws" {
     version = "5.36.0"
-    access_key = "AKIAYZD5AKBTW6UROE6X"
-    secret_key = "SECRET6Cjm7GDnkJInzWvonovD4rBd/jNtnpb2PALMu9bT_KEY"
+    access_key = "ACC"
+    secret_key = "SEC"
     region     = "us-east-1"
 }
