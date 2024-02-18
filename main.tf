3ad833aa91cbfaed023859dbde82f66a38e2ccc5
dev1 <dev1@abc.com>
2024-02-17 22:18:51 -0500

    provider

---
+++ main.tf
@@ -0,0 +1,9 @@
+## Provider's Example
+
+# Configure the AWS Provider
+provider "aws" {
+    version = "5.36.0"
+    access_key = "AKIAYZD5AKBTW6UROE6X"
+    secret_key = "SECRET6Cjm7GDnkJInzWvonovD4rBd/jNtnpb2PALMu9bT_KEY"
+    region     = "us-east-1"
+}
