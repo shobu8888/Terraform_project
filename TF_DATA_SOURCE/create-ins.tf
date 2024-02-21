9ff2c926557667308a0c795ce3e8dab7dc8dcdb2
dev1 <dev1@abc.com>
2024-02-20 23:08:41 -0500

    output

--- TF_DATA_SOURCE/create-ins.tf
+++ TF_DATA_SOURCE/create-ins.tf
@@ -1,13 +1,21 @@
 data "aws_availability_zone" "aval"{
  
+} 
+
+output "private_ip" {
+  value = aws_instance.MyFirstInstnace.private_ip
 }
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           =  lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zone.aval.name[1]
+  availability_zone = data.aws_availability_zone.aval.names[1]
   tags = {
     Name="demoinstance_1"
+  }
+
+  provisioner "local-exec" {
+    command = "echo aws_instance.MyFirstInstnace.public_ip >> abc.txt"
   }
 }
 
