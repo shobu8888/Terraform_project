356e347c7b408ed5b75d192bb1ed320003b71e3e
dev1 <dev1@abc.com>
2024-02-20 22:50:51 -0500

    az_correction

--- TF_DATA_SOURCE/create-ins.tf
+++ TF_DATA_SOURCE/create-ins.tf
@@ -5,9 +5,9 @@ data "aws_availability_zone" "aval"{
 resource "aws_instance" "MyFirstInstnace" {
   ami           =  lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zone.aval.id
+  availability_zone = data.aws_availability_zone.aval.name[1]
   tags = {
-    Name="demoinstance"
+    Name="demoinstance_1"
   }
 }
 
