28cf6a391e14d5312b764352b7195b05a832998a
dev1 <dev1@abc.com>
2024-02-20 23:15:34 -0500

    correction aval zones

--- TF_DATA_SOURCE/create-ins.tf
+++ TF_DATA_SOURCE/create-ins.tf
@@ -1,5 +1,4 @@
-data "aws_availability_zone" "aval"{
- 
+data "aws_availability_zone" "available"{
 } 
 
 output "private_ip" {
@@ -9,7 +8,7 @@ output "private_ip" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           =  lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zone.aval.names[1]
+  availability_zone = data.aws_availability_zone.available.names[1]
   tags = {
     Name="demoinstance_1"
   }
