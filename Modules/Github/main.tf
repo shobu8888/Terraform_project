c6bb455ebee87da136e25ae23250a844975d27d6
dev1 <dev1@abc.com>
2024-02-27 23:48:08 -0500

    ins-cor

--- Modules/Github/main.tf
+++ Modules/Github/main.tf
@@ -1,7 +1,7 @@
 module "ec2-instance" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
     ami = "ami-0b0ea68c435eb488d"
-    instance-type = "t2.micro"
+    instance_type = "t2.micro"
     subnet_id   = "subnet-0ec9bf7cb30d51f25"
 
     tags = {
